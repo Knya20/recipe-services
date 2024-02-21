e74b45b80ebc2fd7288d879cd1d65537fa753404
Knya20 <rurumi.kazuoki@yandex.ru>
2024-02-21 21:07:36 +0300

    Done Observ

--- recipe-service/src/main/java/com/knya/recipeservice/controllers/Controller.java
+++ recipe-service/src/main/java/com/knya/recipeservice/controllers/Controller.java
@@ -4,6 +4,7 @@ import com.knya.recipeservice.dto.RecipeReq;
 import com.knya.recipeservice.models.Recipe;
 import com.knya.recipeservice.services.RecipeService;
 import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -12,6 +13,7 @@ import java.util.List;
 @RestController
 @RequestMapping("/recipe")
 @AllArgsConstructor
+@Slf4j
 public class Controller {
 
     private final RecipeService recipeService;
@@ -23,6 +25,7 @@ public class Controller {
 
     @GetMapping("/findAll")
     public List<Recipe> findAll(){
+        log.info("Запрос на findAll");
         return recipeService.findAll();
     }
 
